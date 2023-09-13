74248c06cec1287bcd0b5e048abe354e69e8495d
weixiang <weixiang06@meituan.com>
2023-09-13 16:15:03 +0800

    fix(JNI coredump): jvm will coredump when JNI is called multiple times - do not call JNI use shell to call segment_builder in another  process

---
+++ java/src/main/java/FileIOUtils.java
@@ -0,0 +1,39 @@
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileIOUtils {
+    public static void packFilesToTarGz(String inputDirPath, String outputFilePath) throws IOException {
+        File inputDir = new File(inputDirPath);
+        File outputFile = new File(outputFilePath);
+
+        try (FileOutputStream fos = new FileOutputStream(outputFile);
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+             GzipCompressorOutputStream gzos = new GzipCompressorOutputStream(bos);
+             TarArchiveOutputStream taos = new TarArchiveOutputStream(gzos)) {
+
+            File[] files = inputDir.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    addFileToTarGz(file, taos);
+                }
+            }
+        }
+    }
+    private static void addFileToTarGz(File file, TarArchiveOutputStream taos) throws IOException {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            TarArchiveEntry entry = new TarArchiveEntry(file, file.getName());
+            taos.putArchiveEntry(entry);
+            IOUtils.copy(fis, taos);
+            taos.closeArchiveEntry();
+        }
+    }
+
+}
--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,26 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.column.ParquetProperties;
-import org.apache.parquet.hadoop.ParquetWriter;
-import org.apache.parquet.hadoop.ParquetFileWriter;
-import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+import org.apache.hadoop.fs.PathFilter;
+import org.apache.spark.SerializableWritable;
 import org.apache.spark.SparkContext;
 import org.apache.spark.TaskContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.catalyst.InternalRow;
-import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder;
-import org.apache.spark.sql.catalyst.encoders.RowEncoder;
-import org.apache.spark.sql.execution.datasources.parquet.ParquetWriteSupport;
-import org.apache.spark.sql.types.StructType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
 
 public class Main {
 
-
-
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
     public static String getHiveDataSQL() {
         String sql = "SELECT `aor_id`, `location_id`, `union_id`, `dt`, `log_type`\n" +
                 "\t, `partition_view`, `first_city_id`, `second_city_id`, `pinhf_batch_id`, `pinhf_biz_type_id`\n" +
@@ -45,58 +53,101 @@ public class Main {
         return sql;
     }
 
+    private static void readProcessOutput(Process process) {
+        read(process.getInputStream(), System.out);
+        read(process.getErrorStream(), System.out);
+    }
+    private static void read(InputStream inputStream, PrintStream out) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-
-
-    public static void main(String[] args) throws InterruptedException {
-        SparkSession sparkSession = SparkSession
-                .builder()
-                .appName("nativeBulkLoad")
-                .enableHiveSupport()
-                .getOrCreate();
-        SparkContext sparkContext = sparkSession.sparkContext();
-        String parquetDstPath = "./parquet/tmp/";
-        Dataset<Row> dataset = sparkSession.sql(getHiveDataSQL());
-        dataset = dataset.repartition(800, dataset.col("union_id"));
-        StructType schema = dataset.schema();
-        dataset.foreachPartition( partition -> {
-            int partitionId = TaskContext.getPartitionId();
-            String parquetFileName = String.format("test_%d.parquet", partitionId);
-            File file = new File(parquetFileName);
-            System.out.println("file path: " + file.getAbsolutePath());
-            ParquetWriteSupport parquetWriteSupport = new ParquetWriteSupport();
-            Configuration conf = new Configuration();
-            conf.setBoolean("spark.sql.parquet.writeLegacyFormat", false);
-            conf.setBoolean("spark.sql.parquet.int64AsTimestampMillis", false);
-            conf.setBoolean("spark.sql.parquet.int96AsTimestamp", true);
-            conf.setBoolean("spark.sql.parquet.binaryAsString", false);
-            conf.set("spark.sql.parquet.outputTimestampType", "INT96");
-            parquetWriteSupport.setSchema(schema, conf);
-            System.out.println(schema);
-            System.out.println(parquetWriteSupport == null);
-            file.deleteOnExit();
-            ParquetWriter<InternalRow> parquetWriter = new ParquetWriter<InternalRow>(new Path("file://" + file.getAbsolutePath()), parquetWriteSupport,
-                    CompressionCodecName.SNAPPY, 256 * 1024 * 1024, 16 * 1024, 1024 * 1024,
-                    false, false,
-                    ParquetProperties.WriterVersion.PARQUET_1_0, conf);
-            System.out.println(parquetWriter == null);
-            ExpressionEncoder<Row> encoder = RowEncoder.apply(schema);
-            while (partition.hasNext()) {
-                // 写入本地 Parquet 文件
-                Row row = partition.next();
-                InternalRow internalRow = encoder.toRow(row);
-//                for (int i = 0; i < schema.length(); i++) {
-//                    internalRow.update(i, row.get(i));
-//                }
-                parquetWriter.write(internalRow);
+            String line;
+            while((line = reader.readLine()) != null) {
+                out.println(line);
             }
-            parquetWriter.close();
-            JniExample jni = new JniExample();
-            jni.segmentBuild("test_tablet_meta", file.getParent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+        }
+
+    }
+
+
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        SparkSession sparkSession = SparkSession.builder().appName("nativeBulkLoad").enableHiveSupport().getOrCreate();
+        SparkContext sparkContext = sparkSession.sparkContext();
+        String parquetDstPath = "/ghnn01/kylin/sparkload/parquet_test/";
+        int TARGET_FILE_SIZE = 524288000;
+        Dataset<Row> dataset = sparkSession.sql(getHiveDataSQL());
+        dataset = dataset.cache();
+        Path path = new Path(parquetDstPath);
+        Configuration conf = sparkContext.hadoopConfiguration();
+        FileSystem fs = path.getFileSystem(conf);
+        FileStatus[] files = fs.listStatus(path, new PathFilter() {
+            public boolean accept(Path path) {
+                return !path.getName().startsWith("_") && !path.getName().startsWith(".");
+            }
         });
-        // sleep 0.5 hour
-        Thread.sleep(1800*1000);
+        List<String> paths = (List) Arrays.stream(files).filter((f) -> {
+            return f.getLen() != 0L;
+        }).map((f) -> {
+            return f.getPath().toString();
+        }).collect(Collectors.toList());
+        int parallel = paths.size();
+        JavaSparkContext jsc = new JavaSparkContext(sparkSession.sparkContext());
+        JavaRDD<String> rdd = jsc.parallelize(paths, parallel);
+        Configuration hadoopConfiguration = jsc.hadoopConfiguration();
+        Broadcast<SerializableWritable<Configuration>> broadcastedHadoopConfig = jsc.broadcast(new SerializableWritable(hadoopConfiguration));
+        rdd.foreachPartition((partition) -> {
+            List<String> fileList = new ArrayList();
+
+            while(partition.hasNext()) {
+                fileList.add(partition.next());
+            }
+
+            FileSystem hadoopFS = (new Path(parquetDstPath)).getFileSystem((Configuration)((SerializableWritable)broadcastedHadoopConfig.value()).value());
+            int stageId = TaskContext.get().stageId();
+            long taskId = TaskContext.get().taskAttemptId();
+            int attemptId = TaskContext.get().attemptNumber();
+            int fileIdx = 0;
+            List<String> targetFilePaths = new ArrayList();
+
+            for (String outputFilePath : fileList) {
+                String localPath = String.format("qarquet_dir/%d/%d/%d/%d/test_parquet.parquet", stageId, taskId, attemptId, fileIdx);
+                File file = new File(localPath);
+                file.deleteOnExit();
+                hadoopFS.copyToLocalFile(new Path(outputFilePath), new Path(file.getAbsolutePath()));
+                String parentDir = file.getParentFile().getAbsolutePath();
+                LOG.info("hdfs file dir:{}, local file dir:{}, local parent dir:{}, local file size:{}", new Object[]{outputFilePath, file.getAbsolutePath(), parentDir, file.getTotalSpace()});
+                ++fileIdx;
+                List<String> cmds = Arrays.asList("./segment_builder", "--meta_file=./test_tablet_meta", String.format("--data_path=%s", parentDir));
+                ProcessBuilder cmdProcess = new ProcessBuilder(cmds);
+                Process process = cmdProcess.start();
+                readProcessOutput(process);
+                int exitCode = process.waitFor();
+                if (exitCode != 0) {
+                    throw new RuntimeException("error when exec cmd:" + cmds);
+                }
+
+                String outputTarFile = String.format("%s/segment_%d_%d_%d_%d.tar.gz", parentDir, stageId, taskId, attemptId, fileIdx);
+                String hdfsoutputTarFile = String.format("/ghnn01/kylin/sparkload/segment_test/segment_%d_%d_%d_%d.tar.gz", stageId, taskId, attemptId, fileIdx);
+                FileIOUtils.packFilesToTarGz(parentDir + "/segment/", outputTarFile);
+                hadoopFS.copyFromLocalFile(new Path(outputTarFile), new Path(hdfsoutputTarFile));
+                targetFilePaths.add(hdfsoutputTarFile);
+            }
+
+            for (String targetFilePath : targetFilePaths) {
+                System.out.println(targetFilePath);
+            }
+        });
+        Thread.sleep(1800000L);
         sparkContext.stop();
     }
 }
